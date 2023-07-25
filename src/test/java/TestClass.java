aaf2cf8d8c7889be49f29ae41cf7820282d43a05
kashi <kashi@192.168.29.218>
2023-07-25 21:45:54 +0530

    First Java Test

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+		
+		By serchTextBox = By.name("q");
+		
+		
+		driver.findElement(serchTextBox).sendKeys("Kashi Swarnkar");
+		driver.findElement(serchTextBox).sendKeys(Keys.RETURN);
+		
+		driver.close();
+		
+		System.out.println("Test Executed Successfully !");
+
+	}
+
+}

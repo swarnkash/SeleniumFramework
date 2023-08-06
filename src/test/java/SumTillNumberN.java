1c3a76123db8793f73f60ba8865a008700642cc1
kashi <kashi@192.168.29.218>
2023-08-06 20:07:50 +0530

    Added Text WOW

---
+++ src/test/java/SumTillNumberN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumTillNumberN {
+
+	public static void main(String[] args) {
+		
+		Scanner Scan = new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		
+		int number = Scan.nextInt();
+		int num = number;
+		int sum = 0;
+
+		for(int i=0; i<=number; i++) {
+			sum = sum +i;
+		}
+		
+		System.out.println("Sum of numbers from 0 to " +num +" is = " +sum);
+		System.out.println("WOW");
+	}
+
+}
